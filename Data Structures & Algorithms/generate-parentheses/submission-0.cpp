3ff1c22b37f0e3d668168cb683d7884569aa2b34
SAKSHIJV2 <145011004+SAKSHIJV2@users.noreply.github.com>
2026-07-06 18:07:35 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(vector<string>& ans,int& n,int& open,int& close,string& curr){
+        if(curr.length()==2*n){
+            ans.push_back(curr);
+            return ;
+        }
+        if(open<n){
+            curr.push_back('(');
+            open++;
+            solve(ans,n,open,close,curr);
+            curr.pop_back();
+            open--;
+        }        
+        if(close<open){
+            curr.push_back(')');
+            close++;
+            solve(ans,n,open,close,curr);
+            curr.pop_back();
+            close--;
+        }        
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        int open=0;
+        int close=0;
+        string curr="";
+        solve(ans,n,open,close,curr);
+        return ans;
+    }
+};
